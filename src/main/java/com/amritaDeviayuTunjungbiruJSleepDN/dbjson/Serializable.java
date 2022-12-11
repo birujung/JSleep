25d97620cbfa2ab32584b68bc544361fe53e1412
birujung <88444195+birujung@users.noreply.github.com>
2022-12-11 16:49:59 +0700

    Update Serializable.java

--- src/main/java/com/amritaDeviayuTunjungbiruJSleepDN/dbjson/Serializable.java
+++ src/main/java/com/amritaDeviayuTunjungbiruJSleepDN/dbjson/Serializable.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 /**
  * A base class for objects that can be serialized and assigned an ID.
  *
- * This class provides an ID field and a simple mechanism for assigning unique IDs to objects of subclasses.
- * It also provides methods for comparing objects by ID and for managing the ID counter for each subclass.
+ * <p>This class provides an ID field and a simple mechanism for assigning unique IDs to objects of subclasses.
+ * It also provides methods for comparing objects by ID and for managing the ID counter for each subclass.</p>
  *
  * @author Amrita Deviayu Tunjungbiru (2106636584)
  * @version 1.0
